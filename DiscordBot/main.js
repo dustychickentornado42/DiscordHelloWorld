3ba1a476408f1edd96299a7931921a51d4f62b4d
Andy Younger <agentandy4@gmail.com>
2022-02-25 18:33:00 -0800

    DiscordHelloWorld

---
+++ DiscordBot/main.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+
+const prefix = '?'
+
+
+client.once('ready', ()=> {
+    console.log('Harambe is online');
+});
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'helloworld'){
+        message.channel.send('Hello World!');
+    }
+});
+
+
+client.login('OTQ2OTQ2NDMxNzc0NzYwOTkw.YhmGlg.4-GqdiNffIh16n30YDxL-Ake-4s');
